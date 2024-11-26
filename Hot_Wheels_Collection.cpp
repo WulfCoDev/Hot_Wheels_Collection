34038e7643c93ed19a9d1b8f618e4d37d122e6f7
WulfCoDev <wulfcodev@gmail.com>
2024-11-25 21:28:16 -0800

    Car Struct and Main Menu

--- Hot_Wheels_Collection.cpp
+++ Hot_Wheels_Collection.cpp
@@ -1,11 +1,54 @@
 #include <iostream>
 #include <string>
-#include <iomanip>
-#include <sstream>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
-int Main(){
+struct Car {
+    string title;
+    string model;
+    string brand;
+    string color;
+    string typeOfWheels;
+    string typeOfVehicle;
+    string toyNumber;
+    string collectorNumber;
+    string seriesNumber;
+    string asstNumber;
+    string modelNumber;
+    string Case;
+    string scale;
+    string barcode;
+};
+
+void DisplayMenu(){
+
+    cout << "\n --- Diecast Collection Tracker --- \n";
+    cout << "1. Add Car \n";
+    cout << "2. Delete Car \n";
+    cout << "3. View Collection \n";
+    cout << "4. Search Menu \n";
+    cout << "5. Load Collection \n";
+    cout << "6. Save Collection \n";
+    cout << "7. Exit \n";
+}
+
+int main(){
+
+    vector <Car> collection;
+    const string FILE_NAME = "Diecast_Collection.txt";
+    int choice;
+
+    do {
+        DisplayMenu();
+        cout << "Choose an option: ";
+        cin >> choice;
+        cin.ignore();
+
+
+    }
+    while (choice != 7);
 
 
     return 0;
